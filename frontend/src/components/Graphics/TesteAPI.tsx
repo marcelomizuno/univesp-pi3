34c6b2046ac5ddb3ecf5e6922d3455b5c0590ad5
Fernanda <fernanda.moura.cruz98@gmail.com>
2025-05-21 15:44:56 -0300

    Testando API

--- frontend/src/components/Graphics/TesteAPI.tsx
+++ frontend/src/components/Graphics/TesteAPI.tsx
@@ -4,19 +4,24 @@ import { useEffect } from "react"
 
 export const TesteAPI = () => {
   useEffect(() => {
-    fetch("https://univesp-pi3.onrender.com/api/v1/tickets/stats/users?period_type=weekly", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+    const url = new URL("https://univesp-pi3.onrender.com/api/v1/tickets/stats/users")
+    url.searchParams.append("period_type", "weekly") // obrigatório
+
+    // Apenas GET, nada de POST aqui!
+    fetch(url.toString(), {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
     })
       .then(async (res) => {
+        const data = await res.json()
+
         if (!res.ok) {
-          const errorData = await res.json()
-          console.error("Erro da API:", errorData)
+          console.error("Erro da API:", data)
           throw new Error("Erro ao buscar dados da API")
         }
-        return res.json()
-      })
-      .then((data) => {
+
         console.log("Funcionando", data)
       })
       .catch((err) => {
